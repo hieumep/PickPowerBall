75a5fc52f53dd9dbfa2307cac76ce8d0b99d4e29
RonaldVo <hieuvo@gmail.com>
2016-01-11 09:46:36 -0800

    Initial Commit

---
+++ PickPowerBall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PickPowerBall
+//
+//  Created by Hieu Vo on 1/11/16.
+//  Copyright © 2016 Hieu Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
