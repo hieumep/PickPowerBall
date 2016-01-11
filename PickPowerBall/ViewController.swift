9818423aaa958054abe54cc6716f32d07fc0f41a
RonaldVo <hieuvo@gmail.com>
2016-01-11 13:28:30 -0800

    complete simple pick random power ball number

--- PickPowerBall/ViewController.swift
+++ PickPowerBall/ViewController.swift
@@ -10,16 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumber: UILabel!
+    @IBOutlet weak var secondNumber: UILabel!
+    @IBOutlet weak var thirdNumber: UILabel!
+    @IBOutlet weak var fourthNumber: UILabel!
+    @IBOutlet weak var fifthNumber: UILabel!
+    @IBOutlet weak var powerBallNumber: UILabel!
+    
+    var arrayNumber : [Int] = []
+    var arrayPowerBallNumber : [Int] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     //   setupArrayNumbers()
     }
 
+    @IBAction func getLuckyNumber(sender: AnyObject) {
+        setupArrayNumbers()
+        firstNumber.text = "\(getRandomNumber())"
+        secondNumber.text = "\(getRandomNumber())"
+        thirdNumber.text = "\(getRandomNumber())"
+        fourthNumber.text = "\(getRandomNumber())"
+        fifthNumber.text = "\(getRandomNumber())"
+        let randomIndex = Int(arc4random_uniform(UInt32(arrayPowerBallNumber.count)))
+        let number = arrayPowerBallNumber[randomIndex]
+        powerBallNumber.text = "\(number)"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setupArrayNumbers(){
+        arrayNumber.removeAll()
+        arrayPowerBallNumber.removeAll()
+        for i in 1...69 {
+            arrayNumber.append(i)
+        }
+        
+        for i in 1...26{
+            arrayPowerBallNumber.append(i)
+        }
+    }
+    
+    func getRandomNumber() -> Int{
+        let randomIndex = Int(arc4random_uniform(UInt32(arrayNumber.count)))
+        let number = arrayNumber[randomIndex]
+        arrayNumber.removeAtIndex(randomIndex)
+        return number
+    }
 }
 
